16f217e8c565e01f912af46d9402de22878db60c
Jounabbas <128884386+Jounabbas@users.noreply.github.com>
2024-03-29 06:10:56 -0700

    requireed some change

--- src/component.js/Home.js
+++ src/component.js/Home.js
@@ -11,16 +11,16 @@ const Home = () => {
   return (
     <>
    <div  className="con">
-      <div className="section1" >
+      <div className="section1"  >
        <Slider/>
       </div>
       
-      <div className="slider" >
-        <img data-aos="slide-right" src="https://img.freepik.com/premium-photo/cyberpunk-gaming-controller-gamepad-joystick-illustration_691560-5812.jpg" alt="" />
-        <img data-aos="slide-up" src="https://img.freepik.com/premium-vector/futuristic-game-pad-video-games-vector-joystick-with-neon-glow-game-console-abstract-geometric-symbols-computer-games-concept-your-design-eps-10_185386-811.jpg" alt="" />
-        <img data-aos="slide-left" src="https://st.depositphotos.com/1013230/2210/i/450/depositphotos_22104925-Game-controller-and-blue-light.jpg" alt="" />
+      <div className="slider">
+        <img  src="https://img.freepik.com/premium-photo/cyberpunk-gaming-controller-gamepad-joystick-illustration_691560-5812.jpg" alt="" />
+        <img  src="https://img.freepik.com/premium-vector/futuristic-game-pad-video-games-vector-joystick-with-neon-glow-game-console-abstract-geometric-symbols-computer-games-concept-your-design-eps-10_185386-811.jpg" alt="" />
+        <img  src="https://st.depositphotos.com/1013230/2210/i/450/depositphotos_22104925-Game-controller-and-blue-light.jpg" alt="" />
       </div>
-      <div className="section2" data-aos="slide-right">
+      <div className="section2" >
         <h1>We Empower Small StartupsTo Turn Into Big Enterprises.</h1>
         <p>
           Our app solutions mutiply your ROI, enhance brand visibilty, and
@@ -29,10 +29,10 @@ const Home = () => {
       </div>
 
       <div className="section3"  >
-        <div id="sec3_1" data-aos="fade-up-right">
+        <div id="sec3_1" >
           <button onClick={()=>navigate("/publishing")}>PUBLISH WITH US</button>
         </div>
-        <div className="sec3_2" data-aos="fade-down-left">
+        <div className="sec3_2" >
           <div className="sec3_2_1">
             <img
               src="https://www.gamedistrict.co/assets/img/icon-marketing.jpg"
@@ -67,7 +67,7 @@ const Home = () => {
         </div>
       </div>
       
-      <div className="sec4" data-aos="slide-up">
+      <div className="sec4" >
         <h1>Our Journey So Far</h1>
         <p>
           Since 2015, we have helped a number of start-ups and big brands
@@ -76,48 +76,48 @@ const Home = () => {
         </p>
       </div>
        
-      <div className="sec5" data-aos="zoom-out">
+      <div className="sec5" >
         <img src="https://i.pinimg.com/736x/8d/85/c4/8d85c4af3912d39f02923312f12f3832.jpg" alt="" />
         
       </div>
 
-      <div className="sec6">
-        <div className="sec6_1" data-aos="fade-up-right">
+      <div className="sec6"  data-aos="fade-right"  >
+        <div className="sec6_1" >
             <img src="https://www.gamedistrict.co/assets/img/icon-published-game.jpg" alt="" />
             <h1>345 <span style={{color:"yellow",fontWeight:"bolder"}}>+</span> </h1>
             <p>Published Games</p>
         </div>
-        <div className="sec6_2" data-aos="fade-down">
+        <div className="sec6_2" >
             <img src="https://www.gamedistrict.co/assets/img/icon-strong-team.jpg" alt="" />
             <h1>345 <span style={{color:"yellow",fontWeight:"bolder"}}>+</span> </h1>
             <p>Published Games</p>
         </div>
-        <div className="sec6_2" data-aos="fade-up">
+        <div className="sec6_2" >
             <img src="https://www.gamedistrict.co/assets/img/icon-projects-delivered.jpg" alt="" />
             <h1>345 <span style={{color:"yellow",fontWeight:"bolder"}}>+</span> </h1>
             <p>Published Games</p>
         </div>
-        <div className="sec6_2" data-aos="zoom-in">
+        <div className="sec6_2" >
             <img src="https://www.gamedistrict.co/assets/img/icon-fortune.jpg" alt="" />
             <h1>345 <span style={{color:"yellow",fontWeight:"bolder"}}>+</span> </h1>
             <p>Published Games</p>
         </div>
-        <div className="sec6_2" data-aos="fade-left">
+        <div className="sec6_2" >
             <img src="https://www.gamedistrict.co/assets/img/icon-raisad.jpg" alt="" />
             <h1>345 <span style={{color:"yellow",fontWeight:"bolder"}}>+</span> </h1>
             <p>Published Games</p>
         </div>
-      <button data-aos="slide-up">Learn More</button>
+      <button >Learn More</button>
       </div>
 
       <div className="sec7">
-        <div className="sec7_1" data-aos="slide-left"><img src="https://www.gamedistrict.co/assets/img/best-clan-left.jpg" alt="" /></div>
-        <div className="sec7_2" data-aos="slide-up">
+        <div className="sec7_1" ><img src="https://www.gamedistrict.co/assets/img/best-clan-left.jpg" alt="" /></div>
+        <div className="sec7_2" >
           <h1>It’s the best clan you can find online</h1>
           <p>Game District longs for fresh & young minds who can mould their approaches with the changing challenges of the digital industry. A growing mind is one of the cherished sights for us as it’s a proof of growth & success in the right direction.</p>
           <button onClick={()=>navigate("/about")}>ABOUT</button>
         </div>
-        <div className="sec7_3" data-aos="slide-left">
+        <div className="sec7_3" >
           <img src="https://www.gamedistrict.co/assets/img/best-clan-right.jpg" alt="" />
         </div>
       </div>
--- src/component.js/Nave.js
+++ src/component.js/Nave.js
@@ -1,43 +1,70 @@
-import React, { useState } from 'react'
-import './Nave.css'
-import { Link, NavLink } from 'react-router-dom';
-import logo from "../component.js/JS.jpeg"
+import React, { useState } from "react";
+import "./Nave.css";
+import { NavLink,Link } from "react-router-dom";
+import logo from "./JS.jpeg"
 const Nave = () => {
-  const [men,setmen]=useState(true);
-  const setmenu=()=>{
-    setmen (!men)
-    console.log(men)
-  }
-  return ( 
+  const [display, setdisplay] = useState(false);
+  const cli = () => {
+    setdisplay(!display);
+  };
+  return (
     <>
-    <div className='header'>
-      <nav>
-            <div className='logo' >
-                <img src={logo}/>
-            </div>
-            <div className='burger' onClick={setmenu}>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-            <div>
-                <ul className={men?"menu":""}> 
-                    <li><NavLink to='/'>HOME</NavLink></li>
-                    <li><NavLink to='/about'>ABOUT</NavLink></li> 
-                    <li><NavLink to='/solution'>SOLUTION</NavLink></li>                                        
-                    <li><NavLink to='/publishing'>PUBLISH</NavLink></li>
-                    <li><NavLink to='/games'>WHY GAMES</NavLink></li>
-                    <li><NavLink to='/career'>CAREER</NavLink></li>
-                    <li><NavLink to='/contact'>CONTACT</NavLink></li>
-                </ul>
-            </div>
-            <div className=' btnn'>
-                <button className={men?"menu":""} >publishing</button>
-            </div>
-      </nav>
+      <div className="containers">
+        <div className="nav">
+          <div className="logo">
+            <img src={logo} alt="" />
+          </div>
+          <div className="hamburger" onClick={cli}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div>
+            <ul>
+              <li><NavLink to='/'>HOME</NavLink></li>
+                      <li><NavLink to='/about'>ABOUT</NavLink></li> 
+                      <li><NavLink to='/solution'>SOLUTION</NavLink></li>                                        
+                      <li><NavLink to='/publishing'>PUBLISH</NavLink></li>
+                      <li><NavLink to='/games'>WHY GAMES</NavLink></li>
+                      <li><NavLink to='/career'>CAREER</NavLink></li>
+                      <li><NavLink to='/contact'>CONTACT</NavLink></li>
+              </ul> 
+          </div>
+          <button>Contact Us</button>
+        </div>
+
+
+        
+      </div>
+
+      <div
+        className="nav2"
+        style={
+          display
+            ? { display: "inline", transition: "display 1s ease" }
+            : { display: "none", transition: "display 1s ease" }
+        }
+      >
+        <div className="logo2" >
+            <img src={logo} alt="" />
+        </div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/project">Project</Link>
+          </li>
+          <li>
+            <Link to="contact">Contact</Link>
+          </li>
+        </ul>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Nave
+export default Nave;
