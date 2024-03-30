fe56d2a6de07c4c18c47c17c9c5b5a49c026c038
Jounabbas <128884386+Jounabbas@users.noreply.github.com>
2024-03-30 06:16:22 -0700

    required some chage

--- src/component.js/Nave.js
+++ src/component.js/Nave.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./Nave.css";
 import logo from "./JS.jpeg"
-import { NavLink,Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navebar = () => {
   const [display, setdisplay] = useState(false);
@@ -48,15 +48,15 @@ const Navebar = () => {
       >
         <div className="logo2" >
             <img src={logo} alt="" />
-        </div>
+        </div> 
           <ul>
-                <li><NavLink to='/'>HOME</NavLink></li>
-                <li><NavLink to='/about'>ABOUT</NavLink></li> 
-                <li><NavLink to='/solution'>SOLUTION</NavLink></li>                                        
-                <li><NavLink to='/publishing'>PUBLISH</NavLink></li>
-                <li><NavLink to='/games'>WHY GAMES</NavLink></li>
-                <li><NavLink to='/career'>CAREER</NavLink></li>
-                <li><NavLink to='/contact'>CONTACT</NavLink></li>
+                <li onClick={()=>setdisplay(!display)}><NavLink to='/'>HOME</NavLink></li>
+                <li onClick={()=>setdisplay(!display)}><NavLink to='/about'>ABOUT</NavLink></li> 
+                <li onClick={()=>setdisplay(!display)}><NavLink to='/solution'>SOLUTION</NavLink></li>                                        
+                <li onClick={()=>setdisplay(!display)}><NavLink to='/publishing'>PUBLISH</NavLink></li>
+                <li onClick={()=>setdisplay(!display)}><NavLink to='/games'>WHY GAMES</NavLink></li>
+                <li onClick={()=>setdisplay(!display)}><NavLink to='/career'>CAREER</NavLink></li>
+                <li onClick={()=>setdisplay(!display)}><NavLink to='/contact'>CONTACT</NavLink></li>
           </ul> 
       </div>
     </>
